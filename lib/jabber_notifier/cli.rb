d449c687e261e106ee8c1ed078ded2a7da8b9cd2
Javier Juarez <javier.juarez@gmail.com>
2011-10-07 21:08:33 +0200

    Afinado el paso de parametros

--- lib/jabber_notifier/cli.rb
+++ lib/jabber_notifier/cli.rb
@@ -1,15 +1,20 @@
+require 'rubygems'
+require 'yaml'
+
+
 module JabberNotifier
   
   class Cli
     
     def initialize(config_file)
       
-      @config ||= YAML.load_file(config_file)
+      config      ||= YAML.load_file(config_file)
+      @connection ||= Connection.new(config[:username], config[:password])
       self
     end
     
-    def notify(options)   
-      Connection.new(@config[:username], @config[:password]).notify(options[:contact], options[:message]).disconnect() 
+    def notify(options)
+      @connection.notify(options[:to], options[:msg]).disconnect
     end
   end
 end
